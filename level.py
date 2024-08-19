976d98e846106bdba0a1fc3d89857d56038222ef
Nicole Pinilla <npinilla89@gmail.com>
2024-08-19 18:56:43 -0400

    se crea funcion para numero de preguntas 2, 3

--- level.py
+++ level.py
@@ -1,17 +1,26 @@
 def choose_level(n_pregunta, p_level):
-    
-    # Construir lógica para escoger el nivel
-    ##################################################
-    pass
-    
-    
-    ##################################################
-    
+    if p_level == 2:
+        if 1 <= n_pregunta <= 2:
+            level = "basico"
+        elif 3 <= n_pregunta <= 4:
+            level = "intermedio"
+        elif 5 <= n_pregunta <= 6:
+            level = "avanzado"
+
+    elif p_level == 3:
+        if 1 <= n_pregunta <= 3:
+            level = "basico"
+        elif 4 <= n_pregunta <= 6:
+            level = "intermedio"
+        elif 7 <= n_pregunta <= 9:
+            level = "avanzado"
+
     return level
+
 
 if __name__ == '__main__':
     # verificar resultados
-    print(choose_level(2, 2)) # básicas
-    print(choose_level(3, 2)) # intermedias
-    print(choose_level(7, 2)) # avanzadas
-    print(choose_level(4, 3)) # intermedias
+    print(choose_level(2, 2))  # básicas
+    print(choose_level(3, 2))  # intermedias
+    print(choose_level(7, 2))  # avanzadas
+    print(choose_level(4, 3))  # intermedias
