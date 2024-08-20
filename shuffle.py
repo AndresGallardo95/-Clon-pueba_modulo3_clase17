3decf8379c9c919e4cbd804d032e3c8970cd6c91
Vanessa Morales N <vane.moralesn@gmail.com>
2024-08-20 18:54:44 -0400

    se incorpora Docstring al archivo shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -2,6 +2,26 @@ import preguntas as p
 import random
 
 def shuffle_alt(pregunta):
+        """  
+    Mezcla aleatoriamente las alternativas de una pregunta dada.  
+
+    Esta función toma una pregunta en forma de diccionario,  
+    extrae sus alternativas y mezcla su orden de manera aleatoria.  
+    La lista original de alternativas se modifica en su lugar.  
+
+    Parámetros:  
+    ----------  
+    pregunta : dict  
+        Un diccionario que representa una pregunta. Debe contener una clave  
+        'alternativas', que se espera sea una lista de listas,  
+        donde cada lista interna representa una alternativa como [opción, valor].  
+
+    Retorna:  
+    -------  
+    list  
+        La lista de alternativas mezcladas de la pregunta de entrada.  
+    """  
+    
     alternativas = (pregunta['alternativas'])
     random.shuffle(alternativas)
     return pregunta['alternativas']
