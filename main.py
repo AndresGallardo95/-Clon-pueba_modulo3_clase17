16edd91c3ffd635efd84205a570c1158e5663a2f
Vane <vane.moralesn@gmail.com>
2024-08-19 21:00:12 -0400

    Se completa código # 5. Escoger el enunciado y las alternativas de una pregunta según el nivel escogido

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ while correcto and n_pregunta < 3*p_level:
         nivel = choose_level()
         print(f'Pregunta {n_pregunta}:')
         # 5. Escoger el enunciado y las alternativas de una pregunta según el nivel escogido
-        enunciado, alternativas = 
+        enunciado, alternativas = choose_q(nivel)
         #6. Imprimir el enunciado y sus alternativas en pantalla
         
         
